cf2adecd6fd42d3eaa88661063bd61ba8c24950f
Erik Telford <e@eriktelford.net>
2016-04-19 01:47:06 -0500

    Only show visible artists

--- app/Http/Controllers/ArtistsController.php
+++ app/Http/Controllers/ArtistsController.php
@@ -11,13 +11,19 @@ class ArtistsController extends Controller
 		if (! $slug) {
 			$artists = Artist::with('instruments')
 							 ->orderBy('lastname')
+							 ->where('visible', 1)
 							 ->get();
 
 			return view('artists', compact('artists'));
 		} else {
 			$artist = Artist::with('instruments')
 							->where('slug', '=', $slug)
+							->where('visible', 1)
 							->first();
+
+			if (is_null($artist)) {
+				return redirect('/');
+			}
 
 			return view('artist-single', compact('artist'));
 		}
